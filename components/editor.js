f18579fe4cfec712f7dbf4ed7985d92f5b86a0b2
mkrebs <michael.krebs@edvschule-plattling.de>
2022-09-30 09:49:19 +0200

    Update editor Look and Feel

--- components/editor.js
+++ components/editor.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Editor.module.css";
 import CodeMirror from "@uiw/react-codemirror";
 import {javascript} from "@codemirror/lang-javascript";
 import {darcula} from '@uiw/codemirror-theme-darcula';
@@ -11,13 +11,20 @@ export default function Editor() {
     return (
         <section className={styles.codemirror}>
             <div className={styles.toolbar}>
-                <select name={"fontStyle"}></select>
+                <button><img src={"/assets/img/bold.svg"} alt={"Mark text bold"}/></button>
+                <button><img src={"/assets/img/italic.svg"} alt={"Mark text bold"}/></button>
+                <button><img src={"/assets/img/underline.svg"} alt={"Mark text bold"}/></button>
+                <select name={"fontFamily"}></select>
                 <select name={"fontSize"}></select>
+                <input type={"color"} name={"fontColor"}/>
+                <button><img src={"/assets/img/alignl.svg"} alt={"Mark text bold"}/></button>
+                <button><img src={"/assets/img/alignc.svg"} alt={"Mark text bold"}/></button>
+                <button><img src={"/assets/img/alignr.svg"} alt={"Mark text bold"}/></button>
             </div>
             <CodeMirror
                 placeholder={"Share the Link to work collaborative. Already shared? Then have fun typing"}
                 height="85vh"
-                extensions={[javascript({jsx: true})]}
+                extensions={[javascript()]}
                 theme={darcula}
                 onChange={onChange}
             /></section>
