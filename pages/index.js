5c3e4d918ad750ee9e78c2c90ae9e3716907dd88
mkrebs <michael.krebs@edvschule-plattling.de>
2022-10-06 13:03:14 +0200

    Outsource Successdialog into external class

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import React from 'react';
 import Editor from "../components/editor";
+import SuccessDialog from "../components/dialogs/successDialog";
 
 export default function Home() {
     return (
@@ -14,6 +15,7 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
+
                 <div className={styles.share}>
                     <button onClick={show} className={"pr-3 pt-2"}><Image src={"/assets/img/share.svg"} alt={"Share"}
                                                                           height={"20px"}
@@ -29,26 +31,8 @@ export default function Home() {
                 </div>
                 <p id={"popup_copied"} className={styles.copied_popup}>Copied</p>
             </main>
+            <SuccessDialog/>
             <Editor/>
-
-            <div className="bg-green-100 p-5 w-full sm:w-1/2 rounded">
-                <div className="flex justify-between">
-                    <div className="flex space-x-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                             className="flex-none fill-current text-green-500 h-4 w-4">
-                            <path
-                                d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/>
-                        </svg>
-                        <div className="flex-1 leading-tight text-sm text-green-700 font-medium">Copied Link</div>
-                    </div>
-                    <button className={styles.closeDialog}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                         className="flex-none fill-current text-green-600 h-3 w-3">
-                        <path
-                            d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/>
-                    </svg></button>
-                </div>
-            </div>
         </div>
     )
 }
