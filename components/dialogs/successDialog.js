a47617df0a6609bdabfcd6a7b5d7980f440dac0c
mkrebs <michael.krebs@edvschule-plattling.de>
2022-10-11 14:32:22 +0200

    Update text of dialog

--- components/dialogs/successDialog.js
+++ components/dialogs/successDialog.js
@@ -1,4 +1,3 @@
-import styles from "../../styles/Home.module.css";
 import React from "react";
 
 export default function SuccessDialog() {
@@ -11,7 +10,7 @@ export default function SuccessDialog() {
                         <path
                             d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.25 16.518l-4.5-4.319 1.396-1.435 3.078 2.937 6.105-6.218 1.421 1.409-7.5 7.626z"/>
                     </svg>
-                    <div className="flex-1 leading-tight text-sm text-green-700 font-medium mt-0.5">Copied Text</div>
+                    <div className="flex-1 leading-tight text-sm text-green-700 font-medium mt-0.5">Copied Link</div>
                 </div>
                 <button className={"p-1.5 cursor-default rounded-xl hover:bg-green-300 active:scale-90 ml-6"}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
