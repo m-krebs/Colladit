c31b8ec3b02d3bf5cb29f04b93280e49c0ff5c3a
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-10-26 09:28:43 +0200

    updated homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,29 +1,18 @@
-import React from "react";
+import React from 'react';
+import '../style/home.css'
 
 function Home() {
   return (
       <div className="home">
         <div className="container">
-          <div className="row align-items-center my-5">
-            <div className="col-lg-7">
-              <img
-                  className="img-fluid rounded mb-4 mb-lg-0"
-                  src="http://placehold.it/900x400"
-                  alt=""
-              />
-            </div>
-            <div className="col-lg-5">
-              <h1 className="font-weight-light">Home</h1>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
-              </p>
-              <label htmlFor={"input-url"}>Input the shared URL or session-ID: </label>
-              <input id={"input-url"} type={'text'}/>
-            </div>
-          </div>
+          <h1 className="font-weight-light">This is the homepage of my Colladit
+            Editor</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book.
+          </p>
         </div>
       </div>
   );
