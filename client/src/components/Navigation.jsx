0bafd4cbfcf1e203fdc351646dd64e04dde21751
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-10-26 09:30:04 +0200

    updated navigation component

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,26 +1,14 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../style/navigation.css';
 
 function Navigation() {
   return (
       <div className="navigation">
-        <nav className="">
-          <div className="container">
-            <NavLink className="navbar-brand" to="/">
-              Colladit
-            </NavLink>
-            <div>
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/session">
-                    New Session
-                    <span className="sr-only">(current)</span>
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <NavLink className="navbar-title" to="/">Home</NavLink>
+        <div className="nav-right">
+          <NavLink to="/session">Editor</NavLink>
+        </div>
       </div>
   );
 }
