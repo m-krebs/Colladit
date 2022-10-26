071d128360aac49de6a05fdeb9aceaac88642d92
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-10-26 09:29:12 +0200

    added route to route exporter

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navigation } from "./Navigation";
 export { default as Home } from "./Home";
-export { default as Session } from './session/Session'
+export { default as Session } from './session/Session';
+export { default as Editor } from './session/Editor';
