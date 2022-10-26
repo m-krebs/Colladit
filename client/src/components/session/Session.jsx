d28513764cbf613a16e22f88ae400d313081c048
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-10-26 09:30:50 +0200

    updated session window

--- client/src/components/session/Session.jsx
+++ client/src/components/session/Session.jsx
@@ -1,12 +1,30 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+import '../../style/session.css';
 
 function Session() {
   return (
       <div className="home">
         <div className="container">
-          <h1 className="text-center mt-5">Thank You for using Colladit</h1>
-          <Outlet />
+          <h1 className="title">Thank You for using Colladit</h1>
+          <div className={'s-container'}>
+            <div>
+              <h3>Existing Session</h3>
+              <label htmlFor="url">Paste link or session-ID: </label>
+              <br/>
+              <input id="url" type={'text'}/>
+              <button>Send</button>
+            </div>
+            <div>
+              <h3>New Session</h3>
+              <button onClick={function(){
+                window.open('http://localhost:3000/session'.concat('/a'),
+                    '_self');
+              }}>CREATE
+              </button>
+            </div>
+          </div>
+          <Outlet/>
         </div>
       </div>
   );
