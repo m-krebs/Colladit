8bf6e6a33ed523055777a0be708fb503eb134f53
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-10-27 12:56:46 +0200

    fixed url generation on new session

--- client/src/components/session/Session.jsx
+++ client/src/components/session/Session.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
 import '../../style/session.css';
+import {v4 as uuid} from 'uuid';
+
+function links() {
+  if((document.location.href).substring(document.location.origin.length)==="/session"){
+  return (<div className={'s-container'}>
+    <div className={'wrapper'}>
+      <div className={'exSession'}>
+        <h3>Existing Session</h3>
+        <label htmlFor="url">Paste link or session-ID: </label>
+        <br/>
+        <input id="url" type={'text'}/>
+        <button>Send</button>
+      </div>
+      <div className={'newSession'}>
+        <h3>New Session</h3>
+        <button onClick={function() {
+          window.open(window.location.origin + '/session/' + uuid(),
+              '_self');
+        }}>CREATE
+        </button>
+      </div>
+    </div>
+  </div>);} else {
+    return <div></div>
+  }
+}
 
 function Session() {
   return (
       <div className="home">
         <div className="container">
           <h1 className="title">Thank You for using Colladit</h1>
-          <div className={'s-container'}>
-            <div>
-              <h3>Existing Session</h3>
-              <label htmlFor="url">Paste link or session-ID: </label>
-              <br/>
-              <input id="url" type={'text'}/>
-              <button>Send</button>
-            </div>
-            <div>
-              <h3>New Session</h3>
-              <button onClick={function(){
-                window.open('http://localhost:3000/session'.concat('/a'),
-                    '_self');
-              }}>CREATE
-              </button>
-            </div>
-          </div>
+          {links()}
           <Outlet/>
         </div>
       </div>
