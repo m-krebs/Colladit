94419266b8706a8fe5a2f91b9a6fe7a928359268
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-07 09:28:58 +0100

    feat: add slug to Editor

--- client/src/components/session/Editor.jsx
+++ client/src/components/session/Editor.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import '../../style/editor.css';
+import {useParams} from 'react-router-dom';
 
 const modules = {
   toolbar: [{ 'font': 'Arial'}, 'bold', 'italic', 'underline', {'direction': 'ltl'}, {'direction': 'ctl'}, {'direction': 'rtl'}]
 }
 
 export default function Editor() {
+  let { sessionSlug } = useParams();
   const [value, setValue] = useState('');
 
-  return <div className={'quill-container'}><ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}/></div>;
+  return <div className={'quill-container'}><a href={'http://localhost:3001/session?id=' + sessionSlug} target={'_blank'} rel={'noopener noreferrer'}><p>{sessionSlug}</p></a><ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}/></div>;
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import {
   Navigation,
   Home,
