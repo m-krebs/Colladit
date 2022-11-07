a1d118d802b9c4bbd1fc41b29ece506be2178ed5
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-07 09:29:16 +0100

    feat: add get route to server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/session', (req, res) => {
+  res.send('id: ' + req.query.id);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
