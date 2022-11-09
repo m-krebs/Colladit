c23af5c90e757ba56e57e173cb36da3b0e6fd787
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-09 09:45:28 +0100

    fix: add cors policy and update post request for session

--- server/index.js
+++ server/index.js
@@ -1,20 +1,32 @@
 import express from "express";
 import {DataStore} from './dataStore.js';
 import {sessions} from './dataStore.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/api/session', (req, res) => {
-  let arr = sessions.find(s=>{return s.id===req.query.id})
-  if(arr===undefined){
+app.use(express.text());
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
+
+app.post('/api/session', (req, res) => {
+  let exArr = sessions.find(s=>{return s.id===req.query.id})
+  if(exArr===undefined){
     sessions.push(new DataStore(req.query.id));
-    res.send(sessions.find(s=>{return s.id===req.query.id}).data)
+    // res.send(sessions.find(s=>{return s.id===req.query.id}).data)
+    res.status(200).send("Created new Session")
     return;
   }
-  console.log(arr)
-  res.send(arr.data);
+  if(req.body.length>0) {
+    exArr.data.length = 0;
+    exArr.data.push(req.body);
+  }
+  console.log(sessions)
+  console.log(exArr.data)
+  res.json(exArr.data);
 });
 
 app.get('/api/sessions', (req, res)=> {
