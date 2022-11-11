de2ed17b44423c62acf2c36556cd45dde63c5aa7
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-11 09:51:36 +0100

    change: remove sync button

--- client/src/components/session/Editor.jsx
+++ client/src/components/session/Editor.jsx
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../../style/editor.css';
-import {useParams} from 'react-router-dom';
 
 const modules = {
   toolbar: [
@@ -16,26 +15,9 @@ const modules = {
 };
 
 export default function Editor() {
-  let {sessionSlug} = useParams();
   const [value, setValue] = useState('');
 
-  useEffect(() => {
-    console.log(value);
-  });
-
-  return <div className={'quill-container'}><a
-      href={'http://localhost:3001/api/session?id=' + sessionSlug}
-      target={'_blank'} rel={'noopener noreferrer'}><p>{sessionSlug}</p></a>
-    <button onClick={function() {
-      fetch('http://localhost:3001/api/session?id=' + sessionSlug, {
-        method: 'POST',
-        body: value,
-      }).then((res) => {
-        res.json().then(data => setValue(JSON.stringify(data)));
-      });
-    }
-    }>Sync
-    </button>
+  return <div className={'quill-container'}>
     <ReactQuill theme="snow" value={value} onChange={setValue}
                 modules={modules}/></div>;
 }
