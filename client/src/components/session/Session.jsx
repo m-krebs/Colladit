999a244bf285e5939e7d0136d5eaa94e84f59a59
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-11 09:54:53 +0100

    feat: add socket client

--- client/src/components/session/Session.jsx
+++ client/src/components/session/Session.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import {Outlet} from 'react-router-dom';
 import '../../style/session.css';
-import {v4 as uuid} from 'uuid';
+import {v4} from 'uuid';
+
+function joinExSession() {
+  let url = document.getElementById("url").value;
+  if(url.includes('/')) {
+    console.log("is with slash: " + url)
+    window.open(url, '_self');
+  } else {
+    console.log("is only uuid: " + url)
+    window.open(window.location.origin + '/session/' + url, '_self');
+  }
+}
+
+function createNewSession() {
+  let uuid = v4();
+  window.open(window.location.origin + '/session/' + uuid,
+      '_self');
+  fetch('http://localhost:3001/api/session/' + uuid, {
+    method: 'POST',
+  }).then();
+}
 
 function links() {
-  if((document.location.href).substring(document.location.origin.length)==="/session"){
+  // if ((document.location.href).substring(document.location.origin.length) ===
+  //     '/session') {
   return (<div className={'s-container'}>
     <div className={'wrapper'}>
       <div className={'exSession'}>
@@ -12,20 +33,18 @@ function links() {
         <label htmlFor="url">Paste link or session-ID: </label>
         <br/>
         <input id="url" type={'text'}/>
-        <button>Send</button>
+        <button onClick={joinExSession}>Send</button>
       </div>
       <div className={'newSession'}>
         <h3>New Session</h3>
-        <button onClick={function() {
-          window.open(window.location.origin + '/session/' + uuid(),
-              '_self');
-          // fetch server
-        }
+        <button onClick={
+          createNewSession
         }>CREATE
         </button>
       </div>
     </div>
-  </div>);}
+  </div>);
+  // }
 }
 
 function Session() {
