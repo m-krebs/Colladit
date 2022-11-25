8cbd8f121d87472e0e89103b68c9c8e229bda314
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-11-25 09:43:45 +0100

    chore: code cleanup

--- client/src/components/session/Editor.jsx
+++ client/src/components/session/Editor.jsx
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../../style/editor.css';
-import { validate } from 'uuid';
+import {validate} from 'uuid';
 
 const modules = {
   toolbar: [
-    [{'font': ['Arial']}],
+    [{'font': ['arial']}],
     [
       'bold',
       'italic',
@@ -19,9 +19,11 @@ const modules = {
 let valid = true;
 
 let ws;
-if (validate(window.location.href.substring(window.location.href.lastIndexOf('/') + 1))) {
-   ws = new WebSocket('ws://localhost:8080?uuid=' +
-      window.location.href.substring(window.location.href.lastIndexOf('/') + 1));
+if (validate(window.location.href.substring(
+    window.location.href.lastIndexOf('/') + 1))) {
+  ws = new WebSocket('ws://localhost:8080?uuid=' +
+      window.location.href.substring(
+          window.location.href.lastIndexOf('/') + 1));
   ws.addEventListener('open', () => {
     ws.send('');
   });
@@ -31,12 +33,11 @@ if (validate(window.location.href.substring(window.location.href.lastIndexOf('/'
 
 function Editor() {
   const [value, setValue] = useState('');
-  if (!valid) return <div><p className={'notValid'}>URL not valid</p></div>
+  if (!valid) return <div><p className={'notValid'}>URL not valid</p></div>;
   ws.onmessage = (event) => {
     console.log('received: ' + event.data);
     setValue(event.data);
   };
-
 
   function handleEditChanges(e) {
     console.log('send: ' + e);
@@ -45,6 +46,7 @@ function Editor() {
   }
 
   return <div className={'quill-container'}>
+    <div ></div>
     <ReactQuill theme="snow"
                 modules={modules} value={value} onChange={handleEditChanges}/>
   </div>;
--- server/index.js
+++ server/index.js
@@ -3,18 +3,18 @@ import {v4} from 'uuid';
 import {parse} from 'url';
 
 const wss = new WebSocketServer({port: 8080});
-wss.on('connection', (ws, req)=>{
+wss.on('connection', (ws, req) => {
   const parameters = parse(req.url, true);
   ws.suid = parameters.query.uuid;
   ws.cuid = v4();
-  ws.on('message', (data)=>{
+  ws.on('message', (data) => {
     console.log('received: %s', data);
     let cnt = 0;
-      wss.clients.forEach((c)=>{
-      if (c.suid===ws.suid && c.cuid !== ws.cuid){
-        console.log("WHY: " + data);
+    wss.clients.forEach((c) => {
+      if (c.suid === ws.suid && c.cuid !== ws.cuid) {
+        console.log('WHY: ' + data);
         cnt++;
-        console.log("send %s times", cnt);
+        console.log('send %s times', cnt);
         c.send(data.toString());
       }
     });
