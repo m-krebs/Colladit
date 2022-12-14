e58282fe84be8f1625447c76ec574f7178fb6ebc
Krebs Michael <michael.krebs@edvschule-plattling.de>
2022-12-14 09:19:09 +0100

    chore: add svg for downloadbtn

--- client/src/components/session/Editor.jsx
+++ client/src/components/session/Editor.jsx
@@ -97,15 +97,14 @@ function Editor() {
         <button onClick={copyUrlToClipboard} className={'s-btn'}>Share this
           session
         </button>
-        <button onClick={downloadTxt} className={'s-btn'}
-                style={{float: 'right'}}><>Download text
-          <svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px'
-               width='12' height='12'
-               viewBox='0 0 50 50'>
-            <path
-                d='M 7 2 L 7 48 L 43 48 L 43 14.59375 L 42.71875 14.28125 L 30.71875 2.28125 L 30.40625 2 Z M 9 4 L 29 4 L 29 16 L 41 16 L 41 46 L 9 46 Z M 31 5.4375 L 39.5625 14 L 31 14 Z M 15 22 L 15 24 L 35 24 L 35 22 Z M 15 28 L 15 30 L 31 30 L 31 28 Z M 15 34 L 15 36 L 35 36 L 35 34 Z'></path>
+        <button onClick={downloadTxt} className={'s-btn r-btn'}
+                style={{float: 'right'}}>
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+               width="24" height="24"
+               viewBox="0 0 24 24">
+            <path d="M 12 4 C 9.6655084 4 7.7006133 5.2494956 6.4296875 7.0136719 C 2.8854572 7.05389 0 9.9465993 0 13.5 C 0 17.078268 2.9217323 20 6.5 20 L 18.5 20 C 21.525577 20 24 17.525577 24 14.5 C 24 11.509638 21.577034 9.0762027 18.599609 9.0195312 C 17.729938 6.1415745 15.152096 4 12 4 z M 12 6 C 14.504527 6 16.55398 7.825491 16.931641 10.214844 L 17.083984 11.175781 L 18.048828 11.050781 C 18.272182 11.021699 18.414903 11 18.5 11 C 20.444423 11 22 12.555577 22 14.5 C 22 16.444423 20.444423 18 18.5 18 L 6.5 18 C 4.0022677 18 2 15.997732 2 13.5 C 2 11.002268 4.0022677 9 6.5 9 C 6.534993 9 6.6164592 9.0069899 6.75 9.0136719 L 7.3613281 9.0449219 L 7.6660156 8.5136719 C 8.5301088 7.0123517 10.137881 6 12 6 z M 11 9 L 11 13 L 8 13 L 12 17 L 16 13 L 13 13 L 13 9 L 11 9 z"></path>
           </svg>
-        </>
+
         </button>
       </div>
       <ReactQuill id={'r-quill'} theme='snow'
